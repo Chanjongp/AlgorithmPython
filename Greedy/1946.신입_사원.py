c668c021678f81d2fb67d7afa2818a5ad9eda73c
Chanjongp <pcj0198@daum.net>
2022-05-21 01:35:59 +0900

    1946. 신입사원 2트

--- Greedy/1946.신입_사원.py
+++ Greedy/1946.신입_사원.py
@@ -1,29 +1,21 @@
 t = int(input())
 
-result = []
+results = []
 
 for _ in range(t):
     n = int(input())
 
     data = [list(map(int, input().split())) for _ in range(n)]
-    data.sort(key=lambda x: x[0])
+    data.sort()
 
-    stack = []
-    start_index = 0
+    result = 1
+    last_index = data[0][1]
+    for i in range(1, n):
+        # 스택 끝에있는 값이 현재 인덱스보다 등수가 작으면
+        if last_index > data[i][1]:
+            last_index = data[i][1]
+            result += 1
 
-    for j in range(0, len(data)):
-        if data[j][1] > data[j + 1][1]:
-            stack.append(data[j])
-            start_index = j + 1
-            break
+    results.append(result)
 
-    for i in range(start_index, len(data)):
-        if stack[0][1] < data[i][1]:
-            stack.clear()
-            stack.append(data[i])
-        else:
-            stack.append(data[i])
-
-    result.append(len(stack))
-
-print("\n".join(str(r) for r in result))
+print("\n".join(str(r) for r in results))
