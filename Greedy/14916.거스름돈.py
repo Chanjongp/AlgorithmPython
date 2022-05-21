e6b75fdb145520664ff925092440e4abecc8b9af
Chanjongp <pcj0198@daum.net>
2022-05-21 15:52:21 +0900

    (그리디)14916 수정

--- Greedy/14916.거스름돈.py
+++ Greedy/14916.거스름돈.py
@@ -1,11 +1,13 @@
+# 5 이하일 경우 케이스를 생각하지 못했음 -> 7트
+
 n = int(input())
 
 count_1, n = divmod(n, 5)
 
 result = -1
-# 5보다 작은 경우
 
 if (n % 2) != 0:
+    # 5보다 작은 경우
     if not count_1 == 0:
         n += 5
         count_1 -= 1
