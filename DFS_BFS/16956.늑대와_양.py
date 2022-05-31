148bd0001001023b27ff8d715c33eb35f2bbbaa4
Chanjongp <pcj0198@daum.net>
2022-06-01 02:32:27 +0900

    (DFS_BFS) 1012

---
+++ DFS_BFS/16956.늑대와_양.py
@@ -0,0 +1,16 @@
+# r, c = map(int, input().split())
+
+# if r > c:
+#     max_v = r
+# else:
+#     max_v = c
+
+# graph = [[] * max_v for _ in range(max_v + 1)]
+# visited = [True] * max_v
+
+# for i in range(1, r + 1):
+#     for j in range(1, c + 1):
+#         v = int(input())
+#         if v == "S":
+#             graph[i].append(j)
+#         else:
---
+++ DFS_BFS/a.py
@@ -0,0 +1,27 @@
+result = 0
+
+
+def dfs(graph, start, value, target):
+    # 절댓값이면 리턴
+    if abs(start) == len(graph):
+        if value == target:
+            global result
+            result += 1
+        return
+
+    value_1 = value + graph[start]
+    value_2 = value - graph[start]
+
+    # # 정수한번 음수한번
+    dfs(graph, start + 1, value_1, target)
+    dfs(graph, start + 1, value_2, target)
+
+
+def solution(numbers, target):
+    dfs(numbers, 0, 0, target)
+
+
+numbers = [1, 1, 1, 1, 1]
+target = 3
+solution(numbers, target)
+print(result)
