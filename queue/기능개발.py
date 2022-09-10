dbee996a8dced2477ab56794e42a9f36a2095bf9
Chanjongp <pcj0198@daum.net>
2022-09-10 22:49:36 +0900

    (프로그래머스) 기능개발

---
+++ queue/기능개발.py
@@ -0,0 +1,34 @@
+"""
+뒤에 있는 기능이 먼저 개발될 수 있음
+이때 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포됩니다.
+
+:: progresses
+:: speeds
+
+100, 100
+"""
+from math import ceil
+
+COMPLETE = 100
+
+
+def solution(progresses, speeds):
+    answer, complete_day = [], []
+
+    complete_day = [
+        ceil((COMPLETE - task) / speed) for task, speed in zip(progresses, speeds)
+    ]
+
+    latest, cnt = complete_day[0], 1
+    size = len(complete_day) - 2
+    for now in complete_day[1:]:
+        # 전에있는 태스크가 크다면 해당 일자에 테스크 완수
+        if latest >= now:
+            cnt += 1
+        else:
+            answer.append(cnt)
+            latest = now
+            cnt = 1
+
+    answer.append(cnt)
+    return answer
